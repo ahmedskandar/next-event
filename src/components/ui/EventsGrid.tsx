360968a20acf1d82a5d52762269f03d9e727bf3b
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-04-28 12:44:29 +0300

    feat: Implement events grid component

---
+++ src/components/ui/EventsGrid.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import React from "react";
+import { CardBody, CardContainer, CardItem } from "./3DCard";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendar, faClock } from "@fortawesome/free-regular-svg-icons";
+import { faCoins, faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+const EventsGrid = ({ events }: { events: TEvent[] }) => {
+  const router = useRouter();
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+      {events.map((item) => (
+        <>
+          <CardContainer>
+            <CardBody className=" relative group/card border-primary-100 w-[22rem] md:w-[23rem] xl:w-[26rem] h-auto rounded-xl p-6 border  ">
+              <div
+                className="cursor-pointer"
+                onClick={() => router.push(`/event/${item.id}`)}
+              >
+                <CardItem
+                  translateZ="50"
+                  className="text-xl font-bold text-primary-500"
+                >
+                  {item.title}
+                </CardItem>
+                <CardItem
+                  as="div"
+                  translateZ="60"
+                  className="text-primary-300 w-full  flex items-center justify-between text-sm mt-2"
+                >
+                  <div className="flex gap-2 items-center basis-1/2">
+                    <FontAwesomeIcon
+                      className="text-primary-500"
+                      icon={faCalendar}
+                    />
+                    <div>
+                      <time>{item.startDate}</time> -
+                      <time> {item.endDate}</time>
+                    </div>
+                  </div>
+                  <div className="flex gap-2 items-center basis-1/2">
+                    <FontAwesomeIcon
+                      className="text-primary-500"
+                      icon={faLocationDot}
+                    />
+                    <span>{item.location}</span>
+                  </div>
+                </CardItem>
+                <CardItem
+                  as="div"
+                  translateZ="60"
+                  className="text-primary-300 w-full gap-2  flex items-center justify-between text-sm mt-2"
+                >
+                  <div className="flex gap-2 items-center basis-1/2">
+                    <FontAwesomeIcon
+                      className="text-primary-500"
+                      icon={faClock}
+                    />
+                    <div>
+                      <time>{item.startTime}</time> -<time>{item.endTime}</time>
+                    </div>
+                  </div>
+                  <div className="flex gap-2 items-center basis-1/2">
+                    <FontAwesomeIcon
+                      className="text-primary-500"
+                      icon={faCoins}
+                    />
+                    <span>{item.price}</span>
+                  </div>
+                </CardItem>
+                <CardItem translateZ="100" className="w-full mt-4">
+                  <Image
+                    src={item.image}
+                    height="500"
+                    width="500"
+                    className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                    alt="thumbnail"
+                  />
+                </CardItem>
+                <CardItem
+                  translateZ={80}
+                  as="p"
+                  href=""
+                  target="__blank"
+                  className=" text-primary-500 mt-3"
+                >
+                  {item.organizer}
+                </CardItem>
+              </div>
+            </CardBody>
+          </CardContainer>
+        </>
+      ))}
+    </div>
+  );
+};
+
+export default EventsGrid;
