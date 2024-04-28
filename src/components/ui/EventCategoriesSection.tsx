c6518dfcd5e2669d269aa27288c8000c85aae2a4
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-04-28 12:07:54 +0300

    feat: Implement event categories section

---
+++ src/components/ui/EventCategoriesSection.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const EventCategoriesSection = () => {
+  return (
+    <section>
+      <h2 className=" my-10 md:my-16 text-3xl font-serif text-primary-500 text-center">
+        Event Categories or FAQ (after Featured Organizers) or testimonials or company about
+      </h2>
+    </section>
+  );
+};
+
+export default EventCategoriesSection;
