84b79d4879011e5fca9f01a9baa650441b128c84
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-04-28 12:09:19 +0300

    style: Change margin

--- src/components/ui/FeaturedOrganizers.tsx
+++ src/components/ui/FeaturedOrganizers.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const FeaturedOrganizers = () => {
   return (
-    <section className='mb-20'>
-      <h2 className="mx-5 m-10 text-3xl font-serif text-primary-500 text-center">
-        Featured Organizers
+    <section className="mb-20">
+      <h2 className=" my-10 md:my-16 text-3xl font-serif text-primary-500 text-center">
+       Featured Organizers
       </h2>
       <div className="flex flex-col md:flex-row gap-8 md:gap-0 mx-10 justify-between items-center py-10 rounded-lg bg-gradient-to-b from-[#FAF3E0] to-[#FEF9E7">
         <div className="flex flex-col items-center justify-center w-full gap-2 md:gap-5">
@@ -17,15 +17,30 @@ const FeaturedOrganizers = () => {
           <h3 className="text-lg text-primary-500">Meta</h3>
         </div>
         <div className="flex flex-col items-center justify-center w-full gap-2 md:gap-5">
-          <Image src="/assets/images/openai.svg" alt="" width={75} height={75} />
+          <Image
+            src="/assets/images/openai.svg"
+            alt=""
+            width={75}
+            height={75}
+          />
           <h3 className="text-lg text-primary-500">OpenAI</h3>
         </div>
         <div className="flex flex-col items-center justify-center w-full gap-2 md:gap-5">
-          <Image src="/assets/images/microsoft.svg" alt="" width={75} height={75} />
+          <Image
+            src="/assets/images/microsoft.svg"
+            alt=""
+            width={75}
+            height={75}
+          />
           <h3 className="text-lg text-primary-500">Microsoft</h3>
         </div>
         <div className="flex flex-col items-center justify-center w-full gap-2 md:gap-5">
-          <Image src="/assets/images/google.svg" alt="" width={75} height={75} />
+          <Image
+            src="/assets/images/google.svg"
+            alt=""
+            width={75}
+            height={75}
+          />
           <h3 className="text-lg text-primary-500">Google</h3>
         </div>
       </div>
